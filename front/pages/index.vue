e2fdbfb59cbd37addf7a23e3beaf1017711960ba
dexia2 <n-koba@fis.nachi-fujikoshi.co.jp>
2018-11-27 18:02:51 +0900

    default to metis

--- front/pages/index.vue
+++ front/pages/index.vue
@@ -1,65 +1,47 @@
 <template>
-  <v-layout
-    column
-    justify-center
-    align-center>
-    <v-flex
-      xs12
-      sm8
-      md6>
-      <div class="text-xs-center">
-        <logo/>
-        <vuetify-logo/>
-      </div>
-      <v-card>
-        <v-card-title class="headline">Welcome to the Vuetify + Nuxt.js template</v-card-title>
-        <v-card-text>
-          <p>Vuetify is a progressive Material Design component framework for Vue.js. It was designed to empower developers to create amazing applications.</p>
-          <p>For more information on Vuetify, check out the <a
-            href="https://vuetifyjs.com"
-            target="_blank">documentation</a>.</p>
-          <p>If you have questions, please join the official <a
-            href="https://chat.vuetifyjs.com/"
-            target="_blank"
-            title="chat">discord</a>.</p>
-          <p>Find a bug? Report it on the github <a
-            href="https://github.com/vuetifyjs/vuetify/issues"
-            target="_blank"
-            title="contribute">issue board</a>.</p>
-          <p>Thank you for developing with Vuetify and I look forward to bringing more exciting features in the future.</p>
-          <div class="text-xs-right">
-            <em><small>&mdash; John Leider</small></em>
-          </div>
-          <hr class="my-3">
-          <a
-            href="https://nuxtjs.org/"
-            target="_blank">Nuxt Documentation</a>
-          <br>
-          <a
-            href="https://github.com/nuxt/nuxt.js"
-            target="_blank">Nuxt GitHub</a>
-        </v-card-text>
-        <v-card-actions>
-          <v-spacer/>
-          <v-btn
-            color="primary"
-            flat
-            nuxt
-            to="/inspire">Continue</v-btn>
-        </v-card-actions>
-      </v-card>
-    </v-flex>
-  </v-layout>
+  <v-data-table 
+    :headers="headers" 
+    :items="books" 
+    class="elevation-1">
+    <template 
+      slot="items" 
+      slot-scope="props">
+      <td>{{ props.item.id }}</td>
+      <td>{{ props.item.name }}</td>
+      <td>{{ props.item.latest }}</td>
+      <td>{{ props.item.period }}</td>
+      <td>{{ props.item.subscribe }}</td>
+      <td>{{ props.item.published }}</td>
+      <td>{{ props.item.author }}</td>
+    </template>
+  </v-data-table>
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
-import VuetifyLogo from '~/components/VuetifyLogo.vue'
-
 export default {
-  components: {
-    Logo,
-    VuetifyLogo
+  data() {
+    return {
+      headers: [
+        { text: 'ID', value: 'id' },
+        { text: '名前', value: 'name' },
+        { text: '最新刊', value: 'latest' },
+        { text: '出版間隔', value: 'period' },
+        { text: '購読サイン', value: 'subscribe' },
+        { text: '出版日', value: 'published' },
+        { text: '作者', value: 'author' }
+      ],
+      books: [
+        {
+          id: 1,
+          name: 'かぐや様は告らせたい~天才たちの恋愛頭脳戦~',
+          latest: 12,
+          period: 3,
+          subscribe: 1,
+          published: '2018-12-19',
+          author: '赤坂アカ'
+        }
+      ]
+    }
   }
 }
 </script>
